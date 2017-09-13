e03ab94f2cda953ba7ee48f4f6ede5170c00a645
FredricM <fmendi@ualberta.ca>
2017-09-12 19:08:38 -0600

    added Tweetable

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
@@ -0,0 +1,14 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+/**
+ * Created by fmendi on 9/12/17.
+ */
+
+//No bodies
+public interface Tweetable {
+    String getMessage();
+    Date getDate();
+
+}
